e95d76bf0fd32a23d6a04809ac53f97515f192ff
Jason Ajax Ayel <62653347+Jason-dev4869@users.noreply.github.com>
2024-12-28 00:17:46 +0700

    Add files via upload

--- Project/src/project/Emp_Flight_Menu.java
+++ Project/src/project/Emp_Flight_Menu.java
@@ -7,13 +7,13 @@ public class Emp_Flight_Menu {
         m.add("Display Flight Board");
         m.add("Quit");
         int choice;
-        Flight f = new Flight();
+        Flights f = new Flights();
         do{
             System.out.println("\n FLIGHT MANAGER");
             choice = m.getChoice();
             switch(choice){
-                case 1: f.Search_Flight(); break;
-                case 2: f.Display_Flight(); break;
+                case 1: //f.Search_Flight(); break;
+                case 2: //f.Display_Flight(); break;
                 case 3: System.exit(0);
             }
         }while(choice >=1 && choice<3);
--- Project/src/project/Flight_Management.java
+++ Project/src/project/Flight_Management.java
@@ -10,14 +10,14 @@ public class Flight_Management {
         menu.add("Display Flight Board");
         menu.add("Quit");
         int choice;
-        Flight f = new Flight();
+        Flights f = new Flights();
         do{
             System.out.println("\n FLIGHT MANAGER");
             choice = menu.getChoice();
             switch(choice){
                 case 1: f.Add_Flight(); break;
                 case 2: f.Remove_Flight(); break;
-                case 3: f.Update_Flight(); break;
+                case 3: //f.Update_Flight(); break;
                 case 4: f.Search_Flight(); break;
                 case 5: f.Display_Flight(); break;
                 case 6: System.exit(0);
