f6a874c05482dc101a3177fab06b5e86fc214cb5
Jason Ajax Ayel <62653347+Jason-dev4869@users.noreply.github.com>
2024-12-28 13:17:01 +0700

    Flight_Management.java

--- Project/src/project/Flight_Management.java
+++ Project/src/project/Flight_Management.java
@@ -17,7 +17,7 @@ public class Flight_Management {
             switch(choice){
                 case 1: f.Add_Flight(); break;
                 case 2: f.Remove_Flight(); break;
-                case 3: //f.Update_Flight(); break;
+                case 3: f.Update_Flight(); break;
                 case 4: f.Search_Flight(); break;
                 case 5: f.Display_Flight(); break;
                 case 6: System.exit(0);
